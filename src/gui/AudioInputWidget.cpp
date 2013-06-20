8bc2ef070b6da0537b6cb99570600102e1186254
Florent Berthaut <florent@hitmuri.net>
2013-06-20 09:23:34 +0200

    Added missing widget files

---
+++ src/gui/AudioInputWidget.cpp
@@ -0,0 +1,24 @@
+/***************************************************************************
+ *  AudioInputWidget.cpp
+ *  2012- Florent Berthaut
+ *  ANR INEDIT Project
+ *  This file is part of LiveTuiles
+ ****************************************************************************/
+
+#include "AudioInputWidget.hpp"
+
+#include <iostream>
+#include <math.h>
+#include <cassert>
+
+#include "../audio/AudioInputTuile.hpp"
+
+using namespace std;
+
+AudioInputWidget::AudioInputWidget(const std::string& name, 
+                        AudioInputTuile* tuile): TuileWidget(name, tuile){}
+
+AudioInputWidget::~AudioInputWidget() {}
+
+
+
